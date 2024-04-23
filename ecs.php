e4341676162c13a5e653d6d1d050b9e768a256ea
Jan Goralski <jan.wojciech.goralski@gmail.com>
2024-04-23 10:04:04 +0200

    [Maintenance] Allow installing Symfony 7

--- ecs.php
+++ ecs.php
@@ -2,12 +2,11 @@
 
 use PhpCsFixer\Fixer\ClassNotation\VisibilityRequiredFixer;
 use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
-use PhpCsFixer\Fixer\Phpdoc\PhpdocTagTypeFixer;
 use SlevomatCodingStandard\Sniffs\Commenting\InlineDocCommentDeclarationSniff;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 
-return static function (ContainerConfigurator $containerConfigurator): void
+return static function (ECSConfig $containerConfigurator): void
 {
     $containerConfigurator->import('vendor/sylius-labs/coding-standard/ecs.php');
 
